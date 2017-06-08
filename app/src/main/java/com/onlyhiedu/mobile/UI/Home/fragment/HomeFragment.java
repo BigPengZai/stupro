24f9a5642e27705d945248335e7cd0a58e2c4e6e
xuweicheng <xuweicheng@onlyedu.com>
2017-06-08 10:31:46 +0800

    -

--- app/src/main/java/com/onlyhiedu/mobile/UI/Home/fragment/HomeFragment.java
+++ app/src/main/java/com/onlyhiedu/mobile/UI/Home/fragment/HomeFragment.java
@@ -1,10 +1,14 @@
 package com.onlyhiedu.mobile.UI.Home.fragment;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
@@ -12,8 +16,11 @@ import com.onlyhiedu.mobile.Base.BaseRecyclerAdapter.OnItemClickListener;
 import com.onlyhiedu.mobile.Base.SimpleFragment;
 import com.onlyhiedu.mobile.Model.bean.HomeNews;
 import com.onlyhiedu.mobile.R;
+import com.onlyhiedu.mobile.UI.Consumption.activity.ConsumeActivity;
+import com.onlyhiedu.mobile.UI.Home.activity.MainActivity;
 import com.onlyhiedu.mobile.UI.Home.adapter.HomeNewsAdapter;
 import com.onlyhiedu.mobile.UI.Home.adapter.TeacherPageAdapter;
+import com.onlyhiedu.mobile.UI.Info.activity.MyInfoActivity;
 import com.onlyhiedu.mobile.Utils.UIUtils;
 import com.onlyhiedu.mobile.Widget.GlideImageLoader;
 import com.youth.banner.Banner;
@@ -22,6 +29,10 @@ import com.youth.banner.listener.OnBannerListener;
 import java.util.ArrayList;
 
 import butterknife.BindView;
+import butterknife.OnClick;
+
+import static com.onlyhiedu.mobile.UI.Home.activity.MainActivity.CALL_REQUEST_CODE;
+import static com.onlyhiedu.mobile.UI.Setting.activity.AboutActivity.PHONE_NUM;
 
 
 /**
@@ -123,6 +134,44 @@ public class HomeFragment extends SimpleFragment implements SwipeRefreshLayout.O
     public void onStop() {
         super.onStop();
         mBanner.stopAutoPlay();
+    }
+
+
+    @OnClick({R.id.tv_hear, R.id.tv_consume, R.id.tv_service, R.id.tv_information})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_hear:
+                UIUtils.startHomeNewsWebViewAct(mContext, urls[0], titles[0]);
+                break;
+            case R.id.tv_consume:
+                startActivity(new Intent(mContext, ConsumeActivity.class));
+                break;
+            case R.id.tv_service:
+                if (UIUtils.requestPermission(mActivity, MainActivity.CALL_REQUEST_CODE, new String[]{Manifest.permission.CALL_PHONE})) {
+                    UIUtils.callLine(mActivity, PHONE_NUM);
+                }
+                break;
+            case R.id.tv_information:
+                startActivity(new Intent(mContext, MyInfoActivity.class));
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CALL_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    UIUtils.callLine(mActivity, PHONE_NUM);
+                } else {
+                    Toast.makeText(mActivity, "拨打电话权限未授权", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
 
