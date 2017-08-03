8170751f33834379c06f1ed38f79320b79badfc9
pengpeng1 <pengpeng1@onlyedu.com>
2017-08-03 17:53:32 +0800

    订单列表UI 添加缩放

--- app/src/main/java/com/onlyhiedu/mobile/UI/Course/adapter/OrderAdapter.java
+++ app/src/main/java/com/onlyhiedu/mobile/UI/Course/adapter/OrderAdapter.java
@@ -1,10 +1,12 @@
 package com.onlyhiedu.mobile.UI.Course.adapter;
 
 import android.content.Context;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.OvershootInterpolator;
 import android.widget.TextView;
 
 import com.onlyhiedu.mobile.Base.BaseRecyclerAdapter;
@@ -50,6 +52,11 @@ public class OrderAdapter extends BaseRecyclerAdapter<OrderList.ListBean> {
                 h.mGoPay.setText("已支付");
                 break;
         }
+        View view = h.itemView;
+        view.setScaleY(0.7f);
+        view.setScaleX(0.7f);
+        ViewCompat.animate(view).scaleX(1.0f).scaleY(1.0f).setDuration(500).setInterpolator(new OvershootInterpolator()).start();
+
     }
 
 
