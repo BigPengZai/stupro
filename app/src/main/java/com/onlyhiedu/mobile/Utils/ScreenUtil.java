8425b3c7e9400910e7f36c0ef7b1ade0934fec0c
pengpeng1 <pengpeng1@onlyedu.com>
2017-08-15 17:45:40 +0800

    修改上课界面 横屏对话提示 显示比例

--- app/src/main/java/com/onlyhiedu/mobile/Utils/ScreenUtil.java
+++ app/src/main/java/com/onlyhiedu/mobile/Utils/ScreenUtil.java
@@ -121,18 +121,7 @@ public class ScreenUtil {
         return false;
     }
 
-    public static  boolean isScreenChange(Context context) {
-        Configuration mConfiguration = context.getResources().getConfiguration(); //获取设置的配置信息
-        int ori = mConfiguration.orientation; //获取屏幕方向
-        if (ori == mConfiguration.ORIENTATION_LANDSCAPE) {
-          //横屏
-            return true;
-        } else if (ori == mConfiguration.ORIENTATION_PORTRAIT) {
-           //竖屏
-            return false;
-        }
-        return false;
-    }
+
 
 
 }
